9f6f1f4868aa34da6b03412d70c113bbd08d29f9
Martin <martin@postnerd.de>
2023-09-08 18:17:45 +0200

    [+] init new home

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+</script>
+
+<main>
+    <div class="container">
+        <h1>Hi, I'm Martin</h1>
+        <p>Code like nobody is watching</p>
+    </div>
+</main>
+
+
+<style>
+    :global(html, body) {
+        margin: 0;
+        padding: 0;
+        background-color: black;
+        color: green;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        font-family: ui-monospace, "Cascadia Mono", "Segoe UI Mono", "Liberation Mono", "Menlo", "Monaco", "Consolas", monospace;
+    }
+</style>
